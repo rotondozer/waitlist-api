e9699afcd46d2865ff7913ee3d6fcd9feb016b1c
rotondozer <nickrotondo724@gmail.com>
2017-10-02 09:10:54 -0400

    Corrects has_many association in Table
    changed from has_many tables to has_many tables_activities

--- app/models/table.rb
+++ app/models/table.rb
@@ -2,5 +2,5 @@ class Table < ApplicationRecord
   # optional: true allows there to be empty fk when creating
   # My db logic depends on this!
   belongs_to :waiting_party, optional: true
-  has_many :tables
+  has_many :tables_activities
 end
