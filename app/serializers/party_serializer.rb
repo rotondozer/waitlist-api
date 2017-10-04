fce824de79b539ffcce5ae6d5e7689c5066687ba
rotondozer <nickrotondo724@gmail.com>
2017-10-04 14:34:31 -0400

    adds est wait to serializer for parties

--- app/serializers/party_serializer.rb
+++ app/serializers/party_serializer.rb
@@ -1,5 +1,5 @@
 class PartySerializer < ActiveModel::Serializer
-  attributes :id, :name, :size, :checked_in, :notes
+  attributes :id, :name, :size, :checked_in, :notes, :est_wait
   has_one :tables_activity
   belongs_to :user
 end
