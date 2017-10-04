2c13c7fc6e183eb59d67dc0c1fb4ac9f5f8ee5e6
rotondozer <nickrotondo724@gmail.com>
2017-10-04 12:29:53 -0400

    Uncomments tables activities resources route group

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/table_activity/:table_number' => 'tables_activities#show_table_activity'
   get '/tables/:party_size/match' => 'tables#match_tables_to_party_size'
   resources :parties
-  # resources :tables_activities
+  resources :tables_activities
   resources :tables
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
