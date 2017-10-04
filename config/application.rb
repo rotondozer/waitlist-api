f518f199e5f2d7aaed8ac402c252e534861214a7
rotondozer <nickrotondo724@gmail.com>
2017-10-04 09:57:48 -0400

    Allows sign in by altering CORS configuration
    Removes comment

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module WaitList
         origins '*'
         resource '*',
                  :headers => :any,
-                #  :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
                  :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
